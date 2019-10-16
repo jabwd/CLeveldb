93115bea48e28584b6aa4bb271b68199e0dc1c5e
Antwan van Houdt <a.vanhoudt@wearetriple.com>
2019-10-16 10:11:45 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,6 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "CLeveldb"
+)
---
+++ shim.h
@@ -0,0 +1,6 @@
+#ifndef LEVELDB_C_H
+#define LEVELDB_C_H
+
+#include <leveldb/c.h>
+
+#endif
